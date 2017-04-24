5f1fae8ace0cb06f4054943084f55db9e1faf94e
alen <yulunxing@126.com>
2017-04-24 15:21:52 +0800

    实例

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ var express = require('express');
 var app = express();
 var routes = require('./routes')(app);
 var temp = require('./routes/temp')(app);
-app.listen(3000);
+var server = app.listen(3000, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("应用实例,访问地址:http://%s:%s", host, port);
+});
